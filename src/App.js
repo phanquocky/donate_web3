079b2d668f7e4ec43e1809091a25d142d407e0ae
phanquocky <phanquocky02@gmail.com>
2023-06-13 15:09:35 +0700

    completed small project withdraw and donate ether to contract

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 
-import { useEffect, useState } from "react";
+import {useCallback, useEffect, useState } from "react";
 import Web3 from 'web3';
 import detectEthereumProvider from "@metamask/detect-provider";
 import { loadContract } from "./utils/loadContract";
@@ -11,21 +11,24 @@ function App() {
     provider: null,
     web3: null,
     contract: null
-  })
-
-  const [account, setAccount] = useState(null)
+  });
+  const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(0);
 
   useEffect(function(){
     const loadProvider = async function() {
       const provider = await detectEthereumProvider();
+      const contract = await loadContract('Faucet', provider)
 
+      // debugger
       if (provider) {
         // console.log('provider', provider)
         // provider.request({method: 'eth_requestAccounts'});
         // console.log('acount', accounts)
         setWeb3Api({
           web3: new Web3(provider),
-          provider
+          provider,
+          contract
         })
       }else{
         console.error("please login to metamask")
@@ -44,16 +47,47 @@ function App() {
     web3Api.web3 && getAccount();
   }, [web3Api.web3])
 
+  useEffect(function() {
+    const loadBalance = async function() {
+      const {contract, web3} = web3Api;
+      console.log(contract.address  )
+      const balance = await web3.eth.getBalance(contract.address);
+      console.log('balance', balance)
+      setBalance(web3.utils.fromWei(balance, "ether"));
+    }
+    console.log('contract', web3Api.contract)
+    web3Api.contract && loadBalance();
+  }, [web3Api])
+
+  const addFunds = useCallback(async ()=> {
+    const {contract, web3} = web3Api;
+    await contract.addFunds({
+      from: account,
+      value: web3.utils.toWei("1", "ether")
+    });
+  }, [web3Api, account])
+
+  const withdraw = async ()=> {
+    const {contract, web3} = web3Api;
+    const withdrawAmount = web3.utils.toWei('0.5', 'ether');
+    await contract.withdraw(withdrawAmount, {
+      from: account
+    })
+  }
 
   return (
-    <div className="container-fluid">
+    <div className="container">
       <div className="container center size" style={{ fontSize: "100px" }}>
         <div className="center">
-          Current balance: <strong>10 ETH</strong>
+          Current balance: <strong>{balance} ETH</strong>
         </div>
         <div className="text-center">
-          <button type="button" className="btn btn-primary mx-2"><h3>Donate</h3></button>
-          <button type="button" className="btn btn-danger mx-2"><h3>WithDraw</h3></button>
+          <button type="button" className="btn btn-primary mx-2" onClick={()=>{
+            addFunds();
+          }}><h3>Donate</h3></button>
+          <button type="button" className="btn btn-danger mx-2" onClick={()=>{
+            withdraw();
+          }}><h3>WithDraw</h3></button>
           <button type="button" className="btn btn-success"
             onClick={()=>{
               web3Api.provider.request({method: 'eth_requestAccounts'});
